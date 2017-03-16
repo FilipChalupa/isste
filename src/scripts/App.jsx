23d2170027cf2c348459a79a0c9b0bf149227713
Filip Chalupa <chalupa.filip@gmail.com>
2017-03-16 22:18:25 +0100

    Red theme

--- src/scripts/App.jsx
+++ src/scripts/App.jsx
@@ -5,6 +5,8 @@ import { Router, hashHistory } from 'react-router'
 import { syncHistoryWithStore } from 'react-router-redux'
 import routes from './_routes'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import { red600, red800 } from 'material-ui/styles/colors'
 import configureStore from './_store'
 
 const store = configureStore()
@@ -13,9 +15,17 @@ const history = syncHistoryWithStore(hashHistory, store)
 import injectTapEventPlugin from 'react-tap-event-plugin'
 injectTapEventPlugin()
 
+const muiTheme = getMuiTheme({
+	palette: {
+    primary1Color: red600,
+    primary2Color: red800,
+    pickerHeaderColor: red600,
+	}
+})
+
 const App = () => (
 	<Provider store={store}>
-		<MuiThemeProvider>
+		<MuiThemeProvider muiTheme={muiTheme}>
 			<Router history={history} routes={routes} />
 		</MuiThemeProvider>
 	</Provider>
